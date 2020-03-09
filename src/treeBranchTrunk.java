0885fa750c356961f136160d424d8c90f0d57012
91070160 <91070160@g.mvusd.net>
2020-03-09 13:30:30 -0700

    Thank you Katie.

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -5,6 +5,7 @@ public class treeBranchTrunk
         System.out.println("The trees are in bloom");
         System.out.println("Mr.Beast planted 20,000,000 trees");
         System.out.println("There are many types are trees");
+        System.out.println("Succulent trees are are nice too");
     }
 
 }//end driver class
